902840498e9ee6ad8acf50ffee1e6079dc510e76
German <gercger5@gmail.com>
2024-05-12 08:51:01 -0300

    Navbar Terminado, agregado fonts

--- webgerman/src/app/layout.tsx
+++ webgerman/src/app/layout.tsx
@@ -1,33 +1,30 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./styles/globals.css";
-import { Inter as FontSans } from "next/font/google"
-import {Roboto, Montserrat} from 'next/font/google'
+import { Inter as FontSans } from "next/font/google";
+import { Roboto, Montserrat } from "next/font/google";
 
-import { cn } from "@/lib/utils"
-import {Navbar} from "@/components/ui/navbar";
-import {NavigationMenu} from "@/components/ui/navigation-menu";
+import { cn } from "@/lib/utils";
+import { Navbar } from "@/components/ui/navbar";
+import { NavigationMenu } from "@/components/ui/navigation-menu";
 import Footer from "@/components/ui/Footer";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
 const roboto_init = Roboto({
   subsets: ["latin"],
-  weight: ['100', '300', '900'],
-  variable: "--font-roboto"
-})
+  weight: ["100", "300", "900"],
+  variable: "--font-roboto",
+});
 
 const montserrat_init = Montserrat({
   subsets: ["latin"],
-  weight: ['100', '300', '400', '600', '700', '900'],
-  variable: "--font-montserrat"
-})
-
-
-
+  weight: ["100", "300", "400", "600", "700", "900"],
+  variable: "--font-montserrat",
+});
 
 export const metadata: Metadata = {
   title: "Web German",
@@ -41,14 +38,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable, roboto_init.variable, montserrat_init.variable)}>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+          roboto_init.variable,
+          montserrat_init.variable
+        )}
+      >
         <div className="sticky top-0 z-10">
-          
-        <Navbar />
+          <Navbar />
         </div>
-        <div className={roboto_init.variable}> 
-          {children}
-        </div>
+        <div className={roboto_init.variable}>{children}</div>
         <Footer />
       </body>
     </html>
--- webgerman/src/app/page.tsx
+++ webgerman/src/app/page.tsx
@@ -1,31 +1,24 @@
-'use client'
+"use client";
 
 import Image from "next/image";
-import foto1 from '@/components/imagenes/foto1jpg.jpg';
-import * as React from "react"
+import foto1 from "@/components/imagenes/foto1jpg.jpg";
+import * as React from "react";
 import Bio from "./bio/page";
 
-
 export default function Home() {
-
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between pb-24 ">
-
-
       <div className="bg-cover bg-center h-screen">
         <Image src={foto1} alt="foto" className="bg-blend-multiply" />
       </div>
 
-      <h1 className="font-montserrat font-normal text-6xl tracking-widest text-slate-50 absolute items-center  pt-64 select-none">HOLA PEPE</h1>
-      
+      <h1 className="font-montserrat font-normal text-6xl tracking-widest text-slate-50 absolute items-center  pt-64 select-none">
+        HOLA PEPE
+      </h1>
+
       <div className="pt-14">
         <Bio />
       </div>
-
-
-
-
-    </main >
+    </main>
   );
 }
